35bdc8cad3f0b083f6f7b626030042056b7cdc18
djlee118 <djlee118@nate.com>
2022-03-04 18:18:16 +0900

    Initial commit

---
+++ src/main/java/com/example/thymeleaf_part01/ThymeleafPart01Application.java
@@ -0,0 +1,13 @@
+package com.example.thymeleaf_part01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafPart01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafPart01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/thymeleaf_part01/ThymeleafPart01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thymeleaf_part01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafPart01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
